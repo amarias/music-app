426eafdd9054bf7d056245f1d6367c6c58336c5c
Allison Marias <allisonmarias@gmail.com>
2019-02-26 01:20:27 -0800

    Test node.js

---
+++ scripts/sounds.js
@@ -0,0 +1,12 @@
+class Sounds {
+    constructor(name, time){
+        this.name = name;
+        this.time = time;
+    }
+
+    getName(){
+        console.log(`My name is ${this.name} and my timestamp is ${this.time}`);
+    }
+}
+
+module.exports = Sounds;
